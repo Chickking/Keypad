17982c59d767187fca54d7554b04f4c00124a2a6
Chickking <me@chickger.pw>
2017-03-12 13:02:04 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const electron = require('electron')
+// 控制应用生命周期的模块。
+const {app} = electron
+// 创建原生浏览器窗口的模块。
+const {BrowserWindow} = electron
+
+// 保持一个对于 window 对象的全局引用，如果你不这样做，
+// 当 JavaScript 对象被垃圾回收， window 会被自动地关闭
+let mainWindow
+
+function createWindow () {
+  // 创建浏览器窗口。
+  mainWindow = new BrowserWindow({width: 413, height: 570, title: 'Keypad', show: false, transparent: true, frame: true})
+  // 加载应用的 index.html。
+  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+
+  // 当 window 被关闭，这个事件会被触发。
+  mainWindow.on('closed', () => {
+    // 取消引用 window 对象，如果你的应用支持多窗口的话，
+    // 通常会把多个 window 对象存放在一个数组里面，
+    // 与此同时，你应该删除相应的元素。
+    mainWindow = null
+  })
+  mainWindow.on('ready-to-show', function() {
+       mainWindow.show();
+       mainWindow.focus();
+   });
+}
+
+// Electron 会在初始化后并准备
+// 创建浏览器窗口时，调用这个函数。
+// 部分 API 在 ready 事件触发后才能使用。
+app.on('ready', createWindow)
+
+// 当全部窗口关闭时退出。
+app.on('window-all-closed', () => {
+  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+  // 否则绝大部分应用及其菜单栏会保持激活。
+  app.quit()
+})
+
+// 在这文件，你可以续写应用剩下主进程代码。
+// 也可以拆分成几个文件，然后用 require 导入。
---
+++ app/main.js
@@ -0,0 +1,68 @@
+
+function getNumber() {
+  var numSharp = window.location.hash;
+  if (numSharp != null) {
+      numSharp = numSharp.substring(1,numSharp.length);
+      var field = document.getElementById('NumberShow');
+      field.value = numSharp;
+  }
+}
+
+function inputNumber(i) {
+  var field = document.getElementById('NumberShow');
+  var au = document.getElementById('keyAudio');
+  field.value = field.value + i;
+  switch (i) {
+    case '*':
+      au.src = "audio/s.wav";
+      au.play();
+      break;
+    case '#':
+      au.src = "audio/p.wav";
+      au.play();
+      break;
+    default:
+      au.src = "audio/" + i + ".wav";
+      au.play();
+      break;
+  }
+}
+
+function btnDelete() {
+  var field = document.getElementById('NumberShow');
+  var strNum = field.value;
+  strNum = strNum.substring(0,strNum.length - 1);
+  field.value = strNum;
+}
+
+function btnCall() {
+  var field = document.getElementById('NumberShow');
+  var strNum = field.value;
+  var telURI = "tel://" + strNum;
+  window.location.href = telURI;
+}
+
+function btnShare() {
+  var qrcode = new QRCode(document.getElementById("qrcode"), {
+	   width : 200,
+	   height : 200
+  });
+  var field = document.getElementById('NumberShow');
+  var strNum = field.value;
+  var telURI = "tel://" + strNum;
+  if (telURI === "tel://") {}
+  else {
+    qrcode.makeCode(telURI);
+    document.getElementById('blur').style = 'display: auto;';
+    document.getElementById('shareQR').style = 'display: auto;';
+  }
+}
+
+/*function saveImage() {
+  var fs = require('fs');
+  fs.fileSave(document.img.src);
+}
+
+function copyImage() {
+
+}*/
